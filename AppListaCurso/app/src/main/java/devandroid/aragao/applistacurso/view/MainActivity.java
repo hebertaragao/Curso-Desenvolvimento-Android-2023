a5ba76cfca371f4809419fc2f6a20d8d36023213
hebertaragao <hebert.aragao@outlook.com>
2023-06-12 20:35:03 -0300

    Criado métodos, atributos da classe Pessoa.java do Model

--- AppListaCurso/app/src/main/java/devandroid/aragao/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/aragao/applistacurso/view/MainActivity.java
@@ -16,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Atribuir conteúdo, dados, valores, para o objeto
         pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Hebert");
+        pessoa.setSobreNome("Aragão");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("(11)999944552273");
+
     }
 }
