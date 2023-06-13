4524b1308b99eca85995c62eaa4bd6a78f823244
hebertaragao <hebert.aragao@outlook.com>
2023-06-12 21:10:40 -0300

    Método toString - POO Herança

--- AppListaCurso/app/src/main/java/devandroid/aragao/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/aragao/applistacurso/model/Pessoa.java
@@ -44,4 +44,16 @@ public class Pessoa {
     public void setTelefoneContato(String telefoneContato) {
         this.telefoneContato = telefoneContato;
     }
+
+    //To String - Para Textos - Para Strings de Caracteres
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                ", telefoneContato='" + telefoneContato + '\'' +
+                '}';
+    }
 }
