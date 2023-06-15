7d856bb20f24734457a1f4d6da21caecb431a41f
hebertaragao <hebert.aragao@outlook.com>
2023-06-15 09:56:33 -0300

    Criado a Classe PessoaController.java na Camanda de Controller - MVC

--- AppListaCurso/app/src/main/java/devandroid/aragao/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/aragao/applistacurso/controller/PessoaController.java
@@ -1,4 +1,19 @@
 package devandroid.aragao.applistacurso.controller;
 
+import androidx.annotation.NonNull;
+
+import devandroid.aragao.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa){
+
+    }
 }
