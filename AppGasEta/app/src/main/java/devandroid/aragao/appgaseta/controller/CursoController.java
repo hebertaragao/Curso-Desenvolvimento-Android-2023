8b3eff38e5a9e2adf4204dc0f03fc48ed5e41a69
hebertaragao <hebert.aragao@outlook.com>
2023-07-11 18:21:36 -0300

    create activity_gaseta.xml

---
+++ AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/controller/CursoController.java
@@ -0,0 +1,37 @@
+package devandroid.aragao.appgaseta.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import devandroid.aragao.appgaseta.model.Curso;
+
+public class CursoController {
+
+    private List listCursos;
+
+    public List getlistaDeCursos(){
+
+        listCursos = new ArrayList<Curso>();
+        listCursos.add(new Curso("Java"));
+        listCursos.add(new Curso("HTML"));
+        listCursos.add(new Curso("C#"));
+        listCursos.add(new Curso("Python"));
+        listCursos.add(new Curso("PHP"));
+        listCursos.add(new Curso("Java EE"));
+        listCursos.add(new Curso("Flutter"));
+        listCursos.add(new Curso("Dart"));
+
+        return listCursos;
+
+    }
+
+    public ArrayList<String> dadosParaSpinner(){
+
+        ArrayList<String> dados = new ArrayList<>();
+        for (int i = 0; i < getlistaDeCursos().size(); i++) {
+            Curso objeto = (Curso) getlistaDeCursos().get(i);
+            dados.add(objeto.getNomeDoCursoDesejado());
+        }
+        return dados;
+    }
+}
