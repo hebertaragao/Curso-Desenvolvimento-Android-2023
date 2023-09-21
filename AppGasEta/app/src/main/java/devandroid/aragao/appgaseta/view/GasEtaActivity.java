411d2ca978a8695252b613806594ff1f83ff5f96
hebertaragao <hebert.aragao@outlook.com>
2023-09-21 20:42:34 -0300

    update GasEtaActivity.java

--- AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/view/GasEtaActivity.java
+++ AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/view/GasEtaActivity.java
@@ -1,26 +1,18 @@
 package devandroid.aragao.appgaseta.view;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.aragao.appgaseta.R;
-import devandroid.aragao.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
-
-    // UtilGasEta utilGasEta = new UtilGasEta();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        // utilGasEta.metodoEstatico();
-        // UtilGasEta.metodoNaoEstatico();
-
-        Toast.makeText(GasEtaActivity.this,UtilGasEta.mensagem(), Toast.LENGTH_LONG);
+        setContentView(R.layout.activity_gaseta);
     }
 }
