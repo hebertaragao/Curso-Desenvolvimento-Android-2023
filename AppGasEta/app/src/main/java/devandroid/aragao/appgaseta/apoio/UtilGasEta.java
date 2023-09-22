40e1628e1696c6d9445ba0e62b257365230d05a1
hebertaragao <hebert.aragao@outlook.com>
2023-09-21 21:01:36 -0300

    update GasEtaActivity.java create UtilGasEta.java

---
+++ AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/apoio/UtilGasEta.java
@@ -0,0 +1,12 @@
+package devandroid.aragao.appgaseta.apoio;
+
+public class UtilGasEta {
+
+    public void metodoEstatico(){}
+
+    public static void metodoNaoEstatico(){}
+
+    public static String mensagem(){
+        return "Qualquer mensagem...";
+    }
+}
--- AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/view/GasEtaActivity.java
+++ AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/view/GasEtaActivity.java
@@ -1,18 +1,27 @@
 package devandroid.aragao.appgaseta.view;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.aragao.appgaseta.R;
+import devandroid.aragao.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
+
+    // UtilGasEta utilGasEta = new UtilGasEta();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_gaseta);
+
+       // UtilGasEta.metodoNaoEstatico();
+       // UtilGasEta.metodoNaoEstatico();
+
+        Toast.makeText(GasEtaActivity.this,UtilGasEta.mensagem(),Toast.LENGTH_LONG).show();
     }
 }
