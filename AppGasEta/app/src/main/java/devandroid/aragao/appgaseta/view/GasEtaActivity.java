f5a89162cfba4d5b52526b0a4dddd29e2f4b6fa9
hebertaragao <hebert.aragao@outlook.com>
2023-09-25 21:05:52 -0300

    implementação btnCalcular

--- AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/view/GasEtaActivity.java
+++ AppGasEta/app/src/main/java/devandroid/aragao/appgaseta/view/GasEtaActivity.java
@@ -1,6 +1,7 @@
 package devandroid.aragao.appgaseta.view;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +25,11 @@ public class GasEtaActivity extends AppCompatActivity {
     Button btnLimpar;
     Button btnSalvar;
     Button btnFinalizar;
+
+    double precoGasolina;
+    double precoEtanol;
+    String recomendacao;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +46,36 @@ public class GasEtaActivity extends AppCompatActivity {
         btnLimpar = findViewById(R.id.btnLimpar);
         btnFinalizar = findViewById(R.id.btnFinalizar);
 
+
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                boolean isDadosOk = true;
+
+                if (TextUtils.isEmpty(editGasolina.getText())) {
+                    editGasolina.setError("* Obrigatório");
+                    editGasolina.requestFocus();
+                    isDadosOk = false;
+                }
+
+                if (TextUtils.isEmpty(editEtanol.getText())) {
+                    editEtanol.setError("* Obrigatório");
+                    editEtanol.requestFocus();
+                    isDadosOk = false;
+                }
+
+                if (isDadosOk) {
+                    // calcular
+                    precoGasolina = Double.parseDouble(editGasolina.getText().toString());
+                    precoEtanol = Double.parseDouble(editEtanol.getText().toString());
+                    recomendacao = UtilGasEta.calcularMelhorOpcao(precoGasolina, precoEtanol);
+                    txtResultado.setText(recomendacao);
+                } else {
+                    Toast.makeText(GasEtaActivity.this,
+                            "Por favor, digite os dados obrigatórios",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -72,6 +104,6 @@ public class GasEtaActivity extends AppCompatActivity {
             }
         });
 
-        Toast.makeText(GasEtaActivity.this,UtilGasEta.calcularMelhorOpcao(5.12,3.39),Toast.LENGTH_LONG).show();
+        Toast.makeText(GasEtaActivity.this, UtilGasEta.calcularMelhorOpcao(5.12, 3.39), Toast.LENGTH_LONG).show();
     }
 }
